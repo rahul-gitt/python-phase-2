90cec4e9c52e8d3319d0e1dd94819f41e7f5e870
rahul-gitt <postbox9635@gmail.com>
2026-06-23 13:19:24 +0530

    A Raise error practice code

---
+++ Exception_handling/raise_error.py
@@ -0,0 +1,9 @@
+try:
+    age = int(input("Enter Your age : "))
+    if age <10 or age>18:
+        raise ValueError("Age must be between 10 and 18")
+    else:
+        print("WWelcome to the club.")
+except Exception as err:
+    print(f"An error occurs as {err}")
+print("Club will start soon")
