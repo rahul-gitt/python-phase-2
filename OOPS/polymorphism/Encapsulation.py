0807954e5e667563bbef86ef7b916c0e54db70dc
rahul-gitt <postbox9635@gmail.com>
2026-06-26 22:47:27 +0530

    Learning about Encapsulation

---
+++ OOPS/polymorphism/Encapsulation.py
@@ -0,0 +1,17 @@
+'''There are 3 types of encapsulation
+    1. Public Attribute and Methods 
+    2. Protected Attributes and Methods
+    3. Private Attributes and methods
+    '''
+
+class Student:
+    def __init__(self):
+        self.name = "rahul"             #Public 
+        self._age = 20                  #protected
+        self.__selary = 100000          # privaye (We can't access it)
+
+obj = Student()
+
+print(obj.name)
+print(obj._age)
+#print(obj.__selary)
